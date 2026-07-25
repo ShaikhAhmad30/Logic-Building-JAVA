677cdfb6d8a48fc8314d53aba1aea036dd3cd2df
Ahmad <shaikhahmad585@gmail.com>
2026-07-25 22:37:25 +0530

    completed Question 006.

---
+++ src/Phase01_Conditional_Thinking/Level01_Simple_Conditions/Question006.java
@@ -0,0 +1,19 @@
+package Phase01_Conditional_Thinking.Level01_Simple_Conditions;
+
+// Question :- Take two numbers and print the larger one.
+
+
+
+
+public class Question006 {
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 30;
+        if(a > b){
+            System.out.println(a + " is greater than " + b );
+        }else{
+            System.out.println(b + " is greater than "+ a);
+        }
+    }
+}
